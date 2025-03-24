8d708438fc250f02b87f134fb3cee710a8d9beee
Rochi2014 <rrodriguez.1991@hotmail.com>
2025-03-23 19:18:31 -0500

    Añadido datos.

---
+++ ejercicio.py
@@ -0,0 +1,23 @@
+def suma():
+    return a+b
+    print(suma(2,3))
+
+def bienvenida(nombre):
+    return('¡Bienvenido a python', nombre+'!')
+
+#print(bienvenida("Pedro"))
+
+def suma2(a,b):
+    print(a+b)
+
+#suma2((4,5))
+
+lenguajes = ["php", "java", "python","ryby", "C", "C++"]
+a=[1,2,3,4,5,6,7,8,9]
+print(len(a))
+print(min(a))
+print(max(a))
+
+print(sum(a))
+print(6 in a)
+print(a.index(3))
